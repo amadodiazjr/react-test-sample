5a83f18157e48a9c5c1ca6fb8cc0a54bd0322c8b
amadodiazjr <amado.diaz@verizonmedia.com>
2019-11-22 11:21:00 -0500

    npm run s-test

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var webdriver = require("selenium-webdriver");
 
 require("geckodriver");// Application Server
 const serverUri = "http://localhost:3000/#";
-const appTitle = "React Selenium App";
+const appTitle = "React App";
 
 /**
  * Config for Chrome browser
@@ -36,17 +36,9 @@ function logTitle() {
    resolve(title);
   });
  });
-}/**
- * Sample test case
- * To check whether the given value is present in array.
- */
-describe("Array", function() {
- describe("#indexOf()", function() {
-  it("should return -1 when the value is not present", function() {
-   assert.equal([1, 2, 3].indexOf(4), -1);
-  });
- });
-});describe("Home Page", function() {
+}
+
+describe("Home Page", function() {
  /**
   * Test case to load our application and check the title.
   */
@@ -61,7 +53,9 @@ describe("Array", function() {
     })
     .catch(err => reject(err));
   });
- });/**
+ });
+ 
+ /**
   * Test case to check whether the given element is loaded.
   */
  it("Should check whether the given element is loaded", function() {
@@ -71,7 +65,9 @@ describe("Array", function() {
     .then(elem => resolve())
     .catch(err => reject(err));
   });
- });/**
+ });
+ 
+ /**
   * End of test cases use.
   * Closing the browser and exit.
   */
